52391789924ee5bd8a678f4bf3ee183c2d918204
Maxxxxxx-x <maxxng1028@gmail.com>
2024-11-18 18:36:49 +0800

    feat: gpx files queries

---
+++ src/db/queries/gpx_files.sql
@@ -0,0 +1,22 @@
+-- name: GetGpxFiles :many
+SELECT * FROM GpxFiles;
+
+-- name: GetGpxFilesById :many
+SELECT * FROM GpxFiles WHERE Id = $1 LIMIT 1;
+
+-- name: GetGpxFilesByUserId :many
+SELECT * FROM GpxFiles
+INNER JOIN Records ON GpxFiles.Id = Records.FileId AND Records.UserId = $1;
+
+-- name: InsertGpxFile :one
+INSERT INTO GpxFiles (
+    Id, FilePath, Checksum
+) VALUES (
+    $1, $2, $3
+) RETURNING *;
+
+-- name: DeleteGpxFileById :exec
+DELETE FROM GpxFiles WHERE Id = $1;
+
+-- name: DeleteGpxFiles :exec
+DELETE FROM GpxFiles
