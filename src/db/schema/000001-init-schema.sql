1d7f894b30ab8cdbe056ab577e714ee28f283f26
Maxxxxxx-x <maxxng1028@gmail.com>
2024-11-22 13:20:39 +0800

    feat: Stats table

--- src/db/schema/000001-init-schema.sql
+++ src/db/schema/000001-init-schema.sql
@@ -22,7 +22,16 @@ CREATE TABLE IF NOT EXISTS Users(
     Id TEXT PRIMARY KEY
 );
 
+CREATE TABLE IF NOT EXISTS UserStats(
+    Id TEXT PRIMARY KEY,
+    UserId TEXT REFERENCES Users(Id);
+    AverageSpd FLOAT,
+    RecordId TEXT REFERENCES Records(Id)
+);
+
 CREATE INDEX IF NOT EXISTS records_userid ON Records(UserId);
 CREATE INDEX IF NOT EXISTS records_fileid ON Records(FileId);
 CREATE INDEX IF NOT EXISTS records_trailname ON Records(TrailName);
 CREATE INDEX IF NOT EXISTS gpxfiles_gpxid ON GpxFiles(GpxId);
+CREATE INDEX IF NOT EXISTS stats_userid ON UserStats(UserId);
+CREATE INDEX IF NOT EXISTS stats_recordid ON UserStats(RecordId);
